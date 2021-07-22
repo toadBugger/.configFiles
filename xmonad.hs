5a567e59fcb500b466474ff1186f27f4b8b09491
toadBugger <toadbugger690@gmail.com>
2021-07-22 07:34:38 +0530

    updated xmonad conf files

--- xmonad.hs
+++ xmonad.hs
@@ -1,11 +1,20 @@
 import XMonad
-import Data.Map (fromList)
-import Data.Monoid (mappend)
+import qualified XMonad.StackSet as W
+import qualified XMonad.Util.CustomKeys as C
+import qualified Data.Map as M
+import XMonad.Actions.SpawnOn
+import XMonad.Util.EZConfig
 
-main  =  xmonad defaultConfig
-      { terminal  		= "kitty"
-      , borderWidth		= 8  
-      , normalBorderColor	= "#ffffff"
-      , focusedBorderColor	= "#e26e0e"
-      }
+main :: IO ()
+main =  xmonad $ defaultConfig
+     { terminal  		= "kitty"
+     , borderWidth		= 5
+     , normalBorderColor	= "#000000"
+     , focusedBorderColor	= "#006400"
+     }
+     `additionalKeys`
+     [ ((mod1Mask, xK_p        ), spawn "dmenu_run -fn 25 -h 35")
+     , ((mod1Mask, xK_b        ), spawn "brave") 
+     ]
+      
 
